d3c9e49665ab28921fbfbf9c784806d9607ba8e9
SunYerim <yaelim6@pukyong.ac.kr>
2024-07-09 23:59:49 +0900

    feat: s3 주소 추가

--- src/main/java/com/sunjoo/auth/global/config/WebConfig.java
+++ src/main/java/com/sunjoo/auth/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000, http://13.124.194.48:9000, https://sunjoo-sand.vercel.app/")
+                .allowedOrigins("http://localhost:3000, http://sunjoo.s3-website.ap-northeast-2.amazonaws.com/, https://sunjoo-sand.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
